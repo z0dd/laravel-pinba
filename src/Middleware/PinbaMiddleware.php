eba13e2cc1db10fa56ce6af0832516bc4259ef8f
z0dd <z0dd@mail.ru>
2019-09-19 20:25:13 +0300

    wip:status

---
+++ src/Middleware/PinbaMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: z0dd
+ */
+
+class PinbaMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
